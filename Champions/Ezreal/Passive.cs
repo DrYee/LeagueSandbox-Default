28967006822b96e33fd0215ac015ce5a932993a8
trick <trick@TRICK-PC>
2017-05-13 23:54:56 +0200

    Ezreal W / Passive

--- Champions/Ezreal/Passive.cs
+++ Champions/Ezreal/Passive.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LeagueSandbox.GameServer.Logic.GameObjects;
 using LeagueSandbox.GameServer.Logic.API;
 using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
 
@@ -12,16 +11,41 @@ namespace Spells
 {
     public class EzrealRisingSpellForce : GameScript
     {
+
+        private GameScriptTimer timer;
+        private ChampionStatModifier mod;
+        private Buff buff;
+        private int n;
+
         public void OnActivate(Champion owner)
         {
-            ApiFunctionManager.LogInfo("Ezreal Passive OnActivate");
-            ApiEventManager.OnChampionDamageTaken.AddListener(this, owner, selfWasDamaged);
+            ApiEventManager.OnAutoAttack.AddListener(owner, OnAutoAttack);
         }
 
-        private void selfWasDamaged()
+        public void OnAutoAttack(Unit owner, Unit target)
         {
-            ApiFunctionManager.LogInfo("Ezreal was damaged");
+            n++;
+            if (n > 5)
+                n = 5;
+
+            if (mod != null)
+                owner.RemoveStatModifier(mod);
+            if (timer != null)
+                timer.EndTimerWithoutCallback();
+
+            mod = new ChampionStatModifier();
+            mod.AttackSpeed.PercentBonus = 0.1f * n;
+            owner.AddStatModifier(mod);
+            buff = ApiFunctionManager.AddBuffHUDVisual("EzrealRisingSpellForce", 6.0f, n, owner);
+            timer = ApiFunctionManager.CreateTimer(6.0f, () =>
+            {
+                owner.RemoveStatModifier(mod);
+                ApiFunctionManager.RemoveBuffHUDVisual(buff);
+                mod = null;
+                n = 0;
+            });
         }
+
         public void OnDeactivate(Champion owner) { }
         public void OnStartCasting(Champion owner, Spell spell, Unit target){}
         public void OnFinishCasting(Champion owner, Spell spell, Unit target) {}
